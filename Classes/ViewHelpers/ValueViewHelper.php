a4306ca42654e6912c00c9adede18e11b5ea79fc
Thomas Nussbaumer <typo3@thomasnu.ch>
2013-04-16 18:55:44 +0200

    [FEATURE] New VH: <tnu:value array=".." key=".." as="..">

---
+++ Classes/ViewHelpers/ValueViewHelper.php
@@ -0,0 +1,71 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2013 Thomas Nussbaumer <typo3@thomasnu.ch>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * View helper with evaluation of PHP code.
+ */
+class Tx_Thomasnu_ViewHelpers_ValueViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+	/**
+	 * Initialize
+	 */
+	public function initializeArguments() {
+		$this->registerArgument('key', 'mixed', 'The key in the array to get the value', TRUE);
+		$this->registerArgument('as', 'string', 'Template variable name to assign. If not specified returns the value instead');
+	}
+	/**
+	 * Searches the array for a given key and returns the corresponding value.
+	 *
+	 * @param array $array The array. 
+	 * @return mixed
+	 */
+	public function render($array = NULL) {
+		$codeWasSource = FALSE;
+		if ($array === NULL) {
+			$array = $this->renderChildren();
+			$codeWasSource = TRUE;
+		}
+		$value = NULL;
+		$key = $this->arguments['key'];
+		if (array_key_exists($key, $array)) {
+			$value = $array[$key];
+		}
+		if ($this->arguments['as']) {
+			if ($this->templateVariableContainer->exists($this->arguments['as'])) {
+				$this->templateVariableContainer->remove($this->arguments['as']);
+			}
+			$this->templateVariableContainer->add($this->arguments['as'], $value);
+			$content = '';
+			if ($codeWasSource === FALSE) {
+				$content = $this->renderChildren();
+				if ($content) {
+					$this->templateVariableContainer->remove($this->arguments['as']);
+				}
+			return $content;
+			}
+		} else {
+			return $value;
+		}
+	}
+}
+?>
--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'thomasNu extbase and fluid',
 	'description' => 's+n edv websites migration to TYPO3 with extbase and fluid. Includes basic configuration and resources. See: www.thomasnu.ch.',
 	'category' => 'plugin',
@@ -32,11 +32,9 @@ $EM_CONF[$_EXTKEY] = array (
 	'author_company' => 's+n edv, CH-6300 Zug',
 	'CGLcompliance' => NULL,
 	'CGLcompliance_note' => NULL,
-	'constraints' => 
-	array (
-		'depends' => 
-		array (
-			'php' => '5.3.7-5.4.99',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.3.7-5.3.99',
 			'typo3' => '6.0.0-6.0.99',
 			'extbase' => '6.0.0-6.0.99',
 			'fluid' => '6.0.0-6.0.99',
